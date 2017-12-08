2fcb8a89488ac0622261ad96d31da9530af1e9d5
Jakub Kulakowski <Kuba@Jakubs-MacBook-Air.local>
2017-12-08 14:15:40 -0500

    new stuff

--- Interactive map/ViewController.swift
+++ Interactive map/ViewController.swift
@@ -7,18 +7,74 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    class customPointAnnotation:MKPointAnnotation {
+        var imageName: String!
+    }
+    @IBOutlet weak var map: MKMapView!
+    
+    let grewenHall = customPointAnnotation()
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations[0]
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.005, 0.005)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(region, animated: true)
+        
+        self.map.showsUserLocation = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        print("New commit")
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        let grewenCoordinates = CLLocationCoordinate2DMake(43.0487734, -76.0875042)
+        grewenHall.coordinate = grewenCoordinates
+        grewenHall.title = "Grewen Hall"
+        grewenHall.imageName = "dolphin.jpg"
+        //grewenHall.subtitle = ""
+        
+        //grewenAnnotation.image = UIImage(named: "dolphin.jpg")
+        map.addAnnotation(grewenHall)
+        
+    }
+    
+    func map(_ map: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?
+    {
+        if !(annotation is MKPointAnnotation) {
+            print("NOT REGISTERED AS MKPOINTANNOTATION")
+            return nil
+        }
+        
+        var annotationView = map.dequeueReusableAnnotationView(withIdentifier: "stationId")
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "stationId")
+            annotationView!.canShowCallout = true
+        }
+            
+        else {
+            annotationView!.annotation = annotation
+        }
+        
+        let cpa = annotation as! customPointAnnotation
+        annotationView!.image = UIImage(named: cpa.imageName)
+        
+        return annotationView
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
